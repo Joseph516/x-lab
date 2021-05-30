12cf26d5673a0253f799b0097efa04d49ea91dbb
yaoyaoyu <ylifebetter@163.com>
2021-05-30 14:02:29 +0800

    user login UI Done

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -7,19 +7,17 @@ const NotFullCalendar = ()=> import('@/components/common/NotFullCalendar.vue')
 const UploadFile = () => import('@/components/common/UploadFile.vue')
 const Home = ()=> import('@/views/home/Home.vue')
 const HomeHeader = ()=> import('@/components/content/HomeHeader.vue')
+//  登录
+const Login = ()=> import('@/views/login/Login.vue')
+
 const Event = ()=> import('@/components/common/Event.vue')
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path:'/',
-    component:UploadFile,
-  },
-  {
-    path:'/home',
-    component:Home,
-  },
+  { path:'/', component:UploadFile,},
+  { path:'/login', component:Login},
+  { path:'/home', component:Home,},
   {
     path:'/homeheader',
     component:HomeHeader
